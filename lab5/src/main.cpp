c7efa458fc3464d2088ea9c3df12c501aa2f5038
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-29 15:21:18 -0600

    Update main.cpp

--- lab5/src/main.cpp
+++ lab5/src/main.cpp
@@ -12,9 +12,6 @@
 //*****************************************************************************
 //Definicion etiquetas
 //*****************************************************************************
-//Botones de contador
-#define Boton1 5
-#define Boton2 21
 
 //Potenciometros
 #define Potenciometro1 15
@@ -28,86 +25,65 @@
 #define en 12 //Sincronización
 #define rs 13 //Selector
 
+//Parámetro PWM led Rojo
+#define pwmChannelLedR 2
+#define freqPWMLedR 5000
+#define resolutionPWMLedR 8
+#define pinPWMLedR 5
+
+//Parámetro PWM led Verde
+#define pwmChannelLedV 1
+#define freqPWMLedV 5000
+#define resolutionPWMLedV 8
+#define pinPWMLedV 18
+
+//Parámetro PWM led Azul
+#define pwmChannelLedA 3
+#define freqPWMLedA 5000
+#define resolutionPWMLedA 8
+#define pinPWMLedA 19
+
 //*****************************************************************************
 //Prototipos de funcion
 //*****************************************************************************
-void configurarBoton1(void);
-void configurarBoton2(void);
+void configurarPWMLedV(void);
+void configurarPWMLedR(void);
+void configurarPWMLedA(void);
 void emaADC(void);
 void valores(void);
-
+void mapeo(void);
+void leds(void);
 //*****************************************************************************
 //Varibles globales
 //*****************************************************************************
-int contadorBoton = 0; //contador de botón
+int rojo = 0;  //Led rojo
+int verde = 0; //Led Verde
+int azul = 0;  //Led azul
 
 //Variables para filtro Medio Móvil Exponencial
-float adcRaw1 = 0.0;     //Valor Crudo potenciómetro 2
-float adcRaw2 = 0.0;     //Valor Crudo potenciómetro 2
-double potFiltrado1 = 0; //S(0) = Y(0)
-double potFiltrado2 = 0; // S(0) = Y(0)
-double alpha = 0.09;     // Factor de suavizado
-float voltage = 0.0;     //Valor de voltaje filtrado
+float adcRaw1 = 0.0;      //Valor Crudo potenciómetro 2
+float adcRaw2 = 0.0;      //Valor Crudo potenciómetro 2
+double rojoFiltrado = 0;  //S(0) = Y(0)
+double verdeFiltrado = 0; // S(0) = Y(0)
+double alpha = 0.09;      // Factor de suavizado
+float voltage = 0.0;      //Valor de voltaje filtrado
 
 //LCD
 LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
-uint8_t decenas1, unidades1, decimal1; //Primer potenciometro
-uint8_t decenas2, unidades2, decimal2; //Segundo potenciometro
-uint8_t centena, decena, unidad;       //Cotnador
+uint8_t centena1, decena1, unidad1; //Led Rojo
+uint8_t centena2, decena2, unidad2; //Led Verde
+uint8_t centena3, decena3, unidad3; //Led Azul
 
-//*****************************************************************************
-//ISR: interrupciones
-//*****************************************************************************
-void IRAM_ATTR ISRBoton1() //interrupción para botón 1 (Aumento)
-{
-  static unsigned long ultimo_tiempo_interrupcion1 = 0; //último tiempo de la interrupción
-  unsigned long tiempo_interrupcion1 = millis();        //tiempo actual de la interrupción
-
-  //Si la interrupcion dura menos de 200ms, asumir que es un rebote e ignorar
-  if (tiempo_interrupcion1 - ultimo_tiempo_interrupcion1 > 200)
-  {
-    contadorBoton++; //aumenta 1 al contador de botón
-
-    if (contadorBoton > 255) //si es mayor a 15 regresa el valor a cero
-    {
-      contadorBoton = 0;
-    }
-  }
-  ultimo_tiempo_interrupcion1 = tiempo_interrupcion1; //actualiza el valor del tiempo de la interrupción
-}
-
-void IRAM_ATTR ISRBoton2() //interrupción para botón 2 (Disminución)
-{
-  static unsigned long ultimo_tiempo_interrupcion2 = 0; //último tiempo de la interrupción
-  unsigned long tiempo_interrupcion2 = millis();        //tiempo actual de la interrupción
-
-  //si la interrupcion dura menos de 200ms, asumir que es un rebote e ignorar
-  if (tiempo_interrupcion2 - ultimo_tiempo_interrupcion2 > 200)
-  {
-    contadorBoton--; //disminuye el contador en 1
-
-    if (contadorBoton < 0) //si es menor a cero, regresa el valor a 4 bits
-    {
-      contadorBoton = 255;
-    }
-  }
-  ultimo_tiempo_interrupcion2 = tiempo_interrupcion2; //actualiza el valor del tiempo de la interrupción
-}
 //*****************************************************************************
 //configuracion
 //*****************************************************************************
 void setup()
 {
+  configurarPWMLedR();
+  configurarPWMLedA();
+  configurarPWMLedV();
+
   Serial.begin(115200);
-
-  //Botones
-  pinMode(Boton1, INPUT_PULLUP);
-  pinMode(Boton2, INPUT_PULLUP);
-
-  //Funciones para configurar los botones
-  configurarBoton1();
-  configurarBoton2();
-
   LCD.begin(16, 2);
 }
 
@@ -116,52 +92,69 @@ void setup()
 //*****************************************************************************
 void loop()
 {
-  emaADC();
+  //emaADC();
+  mapeo();
   valores();
-  Serial.print(potFiltrado1);
-  Serial.println(potFiltrado2);
+  leds();
+
+  Serial.print(rojo);
+  Serial.println(verde);
   LCD.setCursor(0, 0);
-  LCD.print("Pot1: ");
+  LCD.print("Rojo");
   LCD.setCursor(0, 1);
-  LCD.print(decenas1);
-  LCD.print('.');
-  LCD.print(unidades1);
-  LCD.print(decimal1);
-  LCD.print("V");
+  LCD.print(centena1);
+  LCD.print(decena1);
+  LCD.print(unidad1);
 
-  LCD.setCursor(6, 0);
-  LCD.print("Pot2: ");
-  LCD.setCursor(6, 1);
-  LCD.print(decenas2);
-  LCD.print('.');
-  LCD.print(unidades2);
-  LCD.print(decimal2);
-  LCD.print("V");
+  LCD.setCursor(5, 0);
+  LCD.print("Verde");
+  LCD.setCursor(5, 1);
+  LCD.print(centena2);
+  LCD.print(decena2);
+  LCD.print(unidad2);
 
-  LCD.setCursor(12, 0);
-  LCD.print("CPU: ");
-  LCD.setCursor(12, 1);
-  LCD.print(centena);
-  LCD.print(decena);
-  LCD.print(unidad);
+  LCD.setCursor(11, 0);
+  LCD.print("Azul");
+  LCD.setCursor(11, 1);
+  LCD.print(centena3);
+  LCD.print(decena3);
+  LCD.print(unidad3);
 }
 
-//***************************************************
-//Función para configurar Botón 1
-//***************************************************
-void configurarBoton1(void)
+//*****************************************************************************
+//Configuración módulo PWM Led Rojo
+//*****************************************************************************
+void configurarPWMLedR(void)
 {
-  //coloca una interrupción en el botón 1 (durante el cambio de alto a bajo)
-  attachInterrupt(digitalPinToInterrupt(Boton1), ISRBoton1, RISING);
+  //Configurar el modulo PWM
+  ledcSetup(pwmChannelLedR, freqPWMLedR, resolutionPWMLedR);
+
+  //Seleccionar en qué GPIO tendremos nuestra señal PWM
+  ledcAttachPin(pinPWMLedR, pwmChannelLedR);
 }
 
-//***************************************************
-//Función para configurar Botón 2
-//***************************************************
-void configurarBoton2(void)
+//*****************************************************************************
+//Configuración módulo PWM Led Verde
+//*****************************************************************************
+void configurarPWMLedV(void)
 {
-  //coloca una interrupción en el botón 2 (durante el cambio de alto a bajo)
-  attachInterrupt(digitalPinToInterrupt(Boton2), ISRBoton2, RISING);
+  //Configurar el modulo PWM
+  ledcSetup(pwmChannelLedV, freqPWMLedV, resolutionPWMLedV);
+
+  //Seleccionar en qué GPIO tendremos nuestra señal PWM
+  ledcAttachPin(pinPWMLedV, pwmChannelLedV);
+}
+
+//*****************************************************************************
+//Configuración módulo PWM Led Azul
+//*****************************************************************************
+void configurarPWMLedA(void)
+{
+  //Configurar el modulo PWM
+  ledcSetup(pwmChannelLedA, freqPWMLedA, resolutionPWMLedA);
+
+  //Seleccionar en qué GPIO tendremos nuestra señal PWM
+  ledcAttachPin(pinPWMLedA, pwmChannelLedA);
 }
 
 //****************************************************************
@@ -169,40 +162,59 @@ void configurarBoton2(void)
 //****************************************************************
 void emaADC(void)
 {
-  adcRaw1 = analogReadMilliVolts(Potenciometro1) / 10; //toma valor que está midiendo el potenciometro
-  adcRaw2 = analogReadMilliVolts(Potenciometro2) / 10; //toma valor que está midiendo el potenciometro
+  adcRaw1 = analogRead(Potenciometro1); //toma valor que está midiendo el potenciometro
+  adcRaw2 = analogRead(Potenciometro2); //toma valor que está midiendo el potenciometro
 
-  potFiltrado1 = (alpha * adcRaw1) + ((1.0 - alpha) * potFiltrado1); //filtra ese valor
+  //rojoFiltrado = (alpha * adcRaw1) + ((1.0 - alpha) * rojoFiltrado); //filtra ese valor
 
-  potFiltrado2 = (alpha * adcRaw2) + ((1.0 - alpha) * potFiltrado2); //filtra ese valor
+  //verdeFiltrado = (alpha * adcRaw2) + ((1.0 - alpha) * verdeFiltrado); //filtra ese valor
 }
 
 //****************************************************************
-// Dividir valor tomado
+// Función para mapear valores leido a 8 bits
+//****************************************************************
+void mapeo(void)
+{
+  rojo = map(analogRead(Potenciometro1), 0, 4095, 0, 255);
+  verde = map(analogRead(Potenciometro2), 0, 4095, 0, 255);
+}
+
+//****************************************************************
+// Función para tomar valores y dividirlos para LCD
 //****************************************************************
 void valores(void)
 {
   //Primer potenciómetro
-  int temp1 = potFiltrado1;
-  decenas1 = temp1 / 100.0;
-  temp1 = temp1 - decenas1 * 100.0;
-  unidades1 = temp1 / 10.0;
-  temp1 = temp1 - unidades1 * 10.0;
-  decimal1 = temp1;
+  int temp1 = rojo;
+  centena1 = temp1 / 100.0;
+  temp1 = temp1 - centena1 * 100.0;
+  decena1 = temp1 / 10.0;
+  temp1 = temp1 - decena1 * 10.0;
+  unidad1 = temp1;
 
   //Segundo potenciómetro
-  int temp2 = potFiltrado2;
-  decenas2 = temp2 / 100.0;
-  temp2 = temp2 - decenas2 * 100.0;
-  unidades2 = temp2 / 10.0;
-  temp2 = temp2 - unidades2 * 10.0;
-  decimal2 = temp2;
+  int temp2 = verde;
+  centena2 = temp2 / 100.0;
+  temp2 = temp2 - centena2 * 100.0;
+  decena2 = temp2 / 10.0;
+  temp2 = temp2 - decena2 * 10.0;
+  unidad2 = temp2;
 
   //Contador
-  int temp3 = contadorBoton;
-  centena = temp3 / 100.0;
-  temp3 = temp3 - centena * 100.0;
-  decena = temp3 / 10.0;
-  temp3 = temp3 - decena * 10.0;
-  unidad = temp3;
+  int temp3 = azul;
+  centena3 = temp3 / 100.0;
+  temp3 = temp3 - centena3 * 100.0;
+  decena3 = temp3 / 10.0;
+  temp3 = temp3 - decena3 * 10.0;
+  unidad3 = temp3;
+}
+
+//****************************************************************
+// Función para encender LEDS
+//****************************************************************
+void leds(void)
+{
+  ledcWrite(pwmChannelLedR, rojo);
+  ledcWrite(pwmChannelLedV, verde);
+  ledcWrite(pwmChannelLedA, azul);
 }
